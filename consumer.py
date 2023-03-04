f7e5c65df6773280a73ab93a6396c5dc9ce9905d
pierre kasongo <pierrekasongo88@gmail.com>
2023-03-04 16:23:51 +0200

    Adding consumer

---
+++ consumer.py
@@ -0,0 +1,14 @@
+from kafka import KafkaConsumer
+import json
+
+if __name__ == "__main__":
+    consumer = KafkaConsumer(
+        "registered_user",
+        bootstrap_servers = "localhost:9092",
+        auto_offset_reset = "earliest",
+        group_id="consumer-group-a"
+    )
+    print("Starting the consumer")
+
+    for msg in consumer:
+        print("Registered user = {}".format(json.loads(msg.value)))
--- producer.py
+++ producer.py
@@ -7,8 +7,14 @@ from data import get_registered_user
 def json_serializer(data):
     return json.dumps(data).encode("utf-8")
 
+def get_partition(key, all, available):
+    return 0 #Publish only to partition 0
+
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
-                            value_serializer = json_serializer)
+                            value_serializer = json_serializer,
+                            #Uncomment this to publish to specific partition
+                            #partitioner=get_partition
+                        )
 
 
 if __name__ == "__main__":
@@ -16,5 +22,5 @@ if __name__ == "__main__":
         registered_user = get_registered_user()
         print(registered_user)
         #Send to specific topic (topic name should match the broker)
-        producer.send("register_user", registered_user)
+        producer.send("registered_user", registered_user)
         time.sleep(4)
