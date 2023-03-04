302b23c4a257edeb74ea635e5eee9efeac594f35
pierre kasongo <pierrekasongo88@gmail.com>
2023-03-04 20:48:00 +0200

    Adding changes

--- consumer.py
+++ consumer.py
@@ -3,7 +3,7 @@ import json
 
 if __name__ == "__main__":
     consumer = KafkaConsumer(
-        "registered_user",
+        "registered_user", #Topic to listen to
         bootstrap_servers = "localhost:9092",
         auto_offset_reset = "earliest",
         group_id="consumer-group-a"
